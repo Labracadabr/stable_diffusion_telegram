78dc5f3d712ac49489c521f412c8341e184bc1c5
Dmitrii <dminokin@gmail.com>
2024-03-20 13:35:07 +0300

    upd

--- tg/keyboards.py
+++ tg/keyboards.py
@@ -3,6 +3,8 @@ from aiogram.types import KeyboardButton, InlineKeyboardButton as Button, Inline
 # кнопки
 ok: Button = Button(text='✅', callback_data='ok')
 no: Button = Button(text='❌', callback_data='no')
+redo: Button = Button(text='♻️', callback_data='redo')
 
 # клавиатуры из таких кнопок
 keyboard_confirm = Markup(inline_keyboard=[[ok], [no]])
+keyboard_redo = Markup(inline_keyboard=[[redo]])
