ba5fdf9bbc770cc35822ff743815ca688952afa9
Dmitrii <dminokin@gmail.com>
2024-06-17 15:16:17 +0300

    new model stable-diffusion-3

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -1,15 +1,28 @@
 import asyncio
 import os
 import torch
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 from PIL import Image
 
 # model
-model_id = "stabilityai/stable-diffusion-2-1"
+model_id = "stabilityai/stable-diffusion-3-medium"
+# model_id = "stabilityai/stable-diffusion-2-1"
 print('loading model:', model_id)
-torch_dtype = torch.float32
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch_dtype)
-pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+
+# v2
+if model_id == 'stabilityai/stable-diffusion-2-1':
+    from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+    torch_dtype = torch.float32
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch_dtype)
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+
+# v3
+elif model_id == 'stabilityai/stable-diffusion-3-medium':
+    from diffusers import StableDiffusion3Pipeline
+    torch_dtype = torch.float16
+    pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers")
+
+else:
+    exit(f'wrong model {model_id}')
 
 # device: gpu or cpu
 device = "cuda:0" if torch.cuda.is_available else "cpu"
@@ -23,16 +36,15 @@ async def sd_gen(payload: dict) -> Image:
     try:
         # random seed
         generator = torch.manual_seed(payload.get('seed')) if payload.get('seed') else None
+
         image = await asyncio.to_thread(
             lambda: pipe(
                 prompt=payload['pos_prompt'],
                 negative_prompt=payload['neg_prompt'],
                 generator=generator,
                 num_inference_steps=int(payload['steps_num']),
-                resize_method="aspect",
-                # width=payload.get("width"),
-                # height=payload.get("height"),
             ).images[0])
+
     except Exception as e:
         return e
 
@@ -56,23 +68,18 @@ def save_img(image, folder, file_name) -> str:
 
 
 if __name__ == '__main__':
-    x = 0
-    for i in range(50, 51):
-        print(i, 'i')
-        if i not in (50,):
-            continue
 
-        test_payload = {
-            "model_id": model_id,
-            "pos_prompt": "ultra realistic astronaut riding a horse on mars, normal bodies, normal limbs, high quality, spacious desert, moon is seen at the sky ",
-            "neg_prompt": "disfigured, cartoon, anime, painting, low resolution",
-            # "width": 1024,
-            # "height": 800,
-            "steps_num": i,
-            "safety_checker": "yes",
-            "seed": 3
-        }
+    test_payload = {
+        "model_id": model_id,
+        "pos_prompt": "ultra realistic astronaut riding a horse on mars, normal bodies, normal limbs, high quality, spacious desert, moon is seen at the sky ",
+        "neg_prompt": "disfigured, cartoon, anime, painting, low resolution",
+        # "width": 1024,
+        # "height": 800,
+        "steps_num": 5,
+        "safety_checker": "yes",
+        "seed": 3
+    }
 
-        img = sd_gen(payload=test_payload)
-        save_img(img, f'step_{i}')
-        img.show()
+    img = asyncio.run(sd_gen(payload=test_payload))
+    img.show()
+    save_img(img, f'folder', f'img.jpg')
